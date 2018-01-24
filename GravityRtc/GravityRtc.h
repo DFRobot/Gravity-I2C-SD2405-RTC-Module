feeb45a6f94088248a77019d31ed768e861b44ee
Arduinolibrary <grey.cheng@dfrobot.com>
2018-01-24 11:06:23 +0800

    library update

--- GravityRtc/GravityRtc.h
+++ GravityRtc/GravityRtc.h
@@ -20,38 +20,42 @@
 **********************************************************************/
 
 #pragma once
+#include <Arduino.h>
 
 #define RTC_Address   0x32  //RTC_Address 
+
 
 class GravityRtc
 {
 public:
-	GravityRtc();
-	~GravityRtc();
+	GravityRtc() {};
+	~GravityRtc() {};
 
 public:
 	//Year Month Day Weekday Minute Second
-	unsigned int year;
-	unsigned char month;
-	unsigned char day;
-	unsigned char week;
-	unsigned char hour;
-	unsigned char minute;
-	unsigned char second;
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t week;
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
 	
 	//Initialize RTC time to set the corresponding year, month, day, Weekday Minute Second
-	void initRtc(unsigned int year,	unsigned char month,unsigned char day,unsigned char week,
-            	unsigned char hour,unsigned char minute,unsigned char second);
+	void init(const __FlashStringHelper* date, const __FlashStringHelper* time);
+	
+	void adjustRtc(uint16_t year,uint8_t month,uint8_t day,uint8_t week,
+            	uint8_t hour,uint8_t minute,uint8_t second);
 
 	//initialization
 	void setup();
 
 	//Update sensor data
-	void update();
+	void read();
 	
 
 private:
-	unsigned char date[7];
+	uint8_t date[7];
 
 	//Read RTC Time
 	void readRtc();
@@ -60,11 +64,17 @@ private:
 	void processRtc();
 
 	//Decimal turn to BCD
-	char decTobcd(char num);
+	uint8_t decTobcd(uint8_t num);
 	void WriteTimeOn(void);
 	void WriteTimeOff(void);
 	unsigned long timeUpdate;
 
+	//adjust RTC
+	uint8_t dayOfTheWeek();
+	// number of days since 2000/01/01, valid for 2001..2099
+ 	uint16_t date2days(uint16_t y, uint8_t m, uint8_t d);
+	
+	uint8_t conv2d(const char* p);
 
 };
 
--- GravityRtc/examples/RtcTime_test/RtcTime_test.ino
+++ GravityRtc/examples/RtcTime_test/RtcTime_test.ino
@@ -1,20 +1,20 @@
 #include "GravityRtc.h"
 #include "Wire.h"  
 
-
 GravityRtc rtc;     //RTC Initialization
 
 void setup() {
   Serial.begin(9600);
+  
+  //自动设置RTC时钟：通过获取电脑时间校准RTC模块，首先需要保证电脑时间正确
   rtc.setup();
 
-  //rtc.initRtc(2017,6,19,1,12,7,0);  //Set Time: 2017/June/19th/Monday/12:07:0 am
-  //Please comment it after you have set time successfully!!!!!!!
-
+  //手动设置RTC时钟
+  //rtc.adjustRt(2017,6,19,1,12,7,0);  //设置时间：2017/6/19，周一，12点07分0秒
 }
 
 void loop() {
-  rtc.update();
+  rtc.read();
   //*************************Time********************************
   Serial.print("   Year = ");//year
   Serial.print(rtc.year);
